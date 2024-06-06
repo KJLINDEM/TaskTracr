cc1dbed5a0adfc4d300cbeb972a7f5d6c097e16f
Kevin Lindemann <klindemann@ycp.edu>
2024-06-06 22:56:40 +0000

    Modifications to styling. Used :root for defined and consistent styling in user pages. TODO: Modify accountPages.css for better styling. Added media queries and made pages responsive for phones and various desktops.

---
+++ taskManager/src/view/Scripts/newFolder.js
@@ -0,0 +1,22 @@
+document.getElementById('newFolderForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let folderInput = document.getElementById('newFolderInput');
+    const folderName = folderInput.value.trim();
+
+    if(folderName) {
+        const folderRow = document.createElement('div');
+        folderRow.classList.add('row');
+        const folderIcon = document.createElement('img');
+        folderIcon.classList.add('categoryIcon');
+        folderIcon.src = '../Images/Icons/folder.svg';
+        const folderTitle = document.createElement('h3');
+        folderTitle.textContent = folderName;
+
+        //Apply attributes to the row & reset input field.
+        folderRow.appendChild(folderIcon);
+        folderRow.appendChild(folderTitle);
+        document.getElementById('folders').appendChild(folderRow);
+        folderInput.value = '';
+    }
+});
