fa4ccc16f93447756b70d32e0fefd8db6c6aa313
Kevin Lindemann <klindemann@ycp.edu>
2024-06-09 02:47:27 +0000

    Began URL mappings in the homeController.java. Removed unnecessary files

--- taskManager/src/main/java/com/taskTracr/controllers/homeController.java
+++ taskManager/src/main/java/com/taskTracr/controllers/homeController.java
@@ -1,17 +1,31 @@
 package com.taskTracr.controllers;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class homeController {
-    
-    @GetMapping("/") 
-    public String home() {
-        return "Welcome to taskTracr!";
+
+    //DISCUSS: Mappings are to the resources/static directory for Spring Boot
+    //TODO: Is it okay that I include the .html file extension since there is no application.properties to change this? 
+    @GetMapping("/home")
+    public String index() {
+        return "accountPages/home.html";
     }
 
-    @GetMapping("/tasks") 
-    public String tasks() {
-        return "This is the tasks page";
+    //These are the pages for ALL users
+    @GetMapping("/login")
+    public String login() {
+        return "accountPages/login.html";
     }
+
+    @GetMapping("/register")
+    public String register() {
+        return "accountPages/register.html";
+    }
+
+    @GetMapping("/reset-password")
+    public String resetPassword() {
+        return "accountPages/resetPassword.html";
+    }
+
 }
